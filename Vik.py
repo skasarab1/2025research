920f90e1078abc6ea962b1dbef895d65c2a95ba8
Sathvik Kasarabada <sathvik.kas@gmail.com>
2025-04-05 01:58:56 -0500

    new

--- Vik.py
+++ Vik.py
@@ -8,7 +8,8 @@ NYISO = 'NYISO.xlsx'
 UNKNOWN = 'TOBEFILLED.xlsx'
 
 #Read Files
-PSSE = pd.read_excel(PSSE_Data, sheet_name='PSSE_Lines')
+PSSE_Lines = pd.read_excel(PSSE_Data, sheet_name='PSSE_Lines')
+PSSE_Data = pd.read_excel(PSSE_Data, sheet_name='PSSE_Buses')
 Bus_Loc = pd.read_excel(Bus_Locations)
 ISONE = pd.read_excel(ISOne)
 Unknown = pd.read_excel(UNKNOWN)
@@ -21,24 +22,50 @@ Unknown['Closest Lat'] = None
 Unknown['Closest Lon'] = None
 Unknown['N Levels'] = None
 
-for idx, row in Unknown.head(4).iterrows():
+#Bus Name from Bus Number
+def get_bus_name(bus_number, psse_df):
+    # Look for the bus number in either From or To columns
+    match = psse_df[
+        (psse_df['Bus  Number'] == bus_number)
+    ]
+
+    if not match.empty:
+        # Return the matching name based on which side the number was found
+        row = match.iloc[0]
+        if row['Bus  Number'] == bus_number:
+            return row['Bus  Name']
+
+for idx, row in Unknown.iterrows():
     BusNum = row['Bus  Number'] #Bus Number from Unknown Data set
     BusName = row['Bus  Name'] #Bus Name from Unknown Data set
 
-    MatchingRows = PSSE[
-        (PSSE['From Bus  Number'] == BusNum) |
-        (PSSE['To Bus  Number'] == BusNum)
+    MatchingRows = PSSE_Lines[
+        (PSSE_Lines['From Bus  Number'] == BusNum) |
+        (PSSE_Lines['To Bus  Number'] == BusNum)
     ]
 
-    for idx, line in MatchingRows.iterrows():
-        ConnectedBuses = [] #empty data set for found buses
+    ConnectedBuses = [] #empty data set for found buses
+
+    for _, line in MatchingRows.iterrows():
+
         if line['From Bus  Number'] == BusNum:
             ConnectedBuses.append(line['To Bus  Number'])
         else:
             ConnectedBuses.append(line['From Bus  Number'])
         
-    NumConnections = len(set(ConnectedBuses))
-    print(NumConnections)
+    NumConnections = len(set(ConnectedBuses)) # Number of Connections
+
+    if ConnectedBuses:
+        target_bus_num = ConnectedBuses[0] #first row 
+        Unknown.at[idx, 'Closest Name'] = get_bus_name(target_bus_num,PSSE_Data)
+        match = Bus_Loc[Bus_Loc['BusNumber'] == target_bus_num]
+
+        if not match.empty:
+            Unknown.at[idx, 'Closest Lat'] = match.iloc[0]['Lat']
+            Unknown.at[idx, 'Closest Lon'] = match.iloc[0]['Long']
+
+
+Unknown.to_excel('help.xlsx')
     
 
 
